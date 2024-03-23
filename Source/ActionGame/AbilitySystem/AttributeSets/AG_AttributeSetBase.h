650d228a3240b7c3706f1e839caa1b0162d85117
zzzzz0807@gmail.com <zzzzz0807@gmail.com>
2024-03-24 00:53:07 +0900

    <섹션 3> (12) 파일추가 진행 중

---
+++ Source/ActionGame/AbilitySystem/AttributeSets/AG_AttributeSetBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AG_AttributeSetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAME_API UAG_AttributeSetBase : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
